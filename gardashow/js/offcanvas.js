102137c8387ead8a686298fe34431e4814150054
Pavel Balandin <pavelbalandin@gmail.com>
2022-06-14 22:38:34 +0300

    Delete gardashow directory

--- gardashow/js/offcanvas.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
-  });
-
-  $('.list-group .list-group-item').click(function(){
-    $('.row-offcanvas-left').removeClass('active');
-  })
-});
